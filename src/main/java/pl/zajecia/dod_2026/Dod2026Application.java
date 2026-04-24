2442be4077f5b48fa0fe89cf68cb03d2c145e409
KatarzynaGawrys <katarzyna.gawrys@polsl.pl>
2026-04-24 09:33:23 +0200

    Initial commit

---
+++ src/main/java/pl/zajecia/dod_2026/Dod2026Application.java
@@ -0,0 +1,13 @@
+package pl.zajecia.dod_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dod2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dod2026Application.class, args);
+    }
+
+}
